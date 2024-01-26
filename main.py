b348b3f0dbdc367c47630ff688ea28044cffb283
cavalieribelangerr <143746287+cavalieribelangerr@users.noreply.github.com>
2024-01-26 11:37:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ COLORS = []
 
 
 
-
+# classe pour les balles
 class Balle:
     def __init__(self, x, y, change_x, change_y, rayon, couleur):
         self.x = x
@@ -17,6 +17,7 @@ class Balle:
         self.change_y = random.randint(-10, 10)
         self.rayon = rayon
         self.couleur = couleur
+    # méthode qui modifie la direction des balles par rapport aux contact avec les murs
     def update(self):
         self.x +=self.change_x
         self.y += self.change_y
@@ -30,10 +31,11 @@ class Balle:
         elif self.y > SCREEN_HEIGHT:
             self.change_y -= 2*self.change_y
 
-
+    # Méthode qui dessine les balles à l'écran
     def draw(self):
         arcade.draw_circle_filled(self.x, self.y, self.rayon, self.couleur)
 
+# méthode pour les rectangle
 class Rectangle:
     def __init__(self, x, y, width, height,couleur, tilt_angle):
         self.x = x
@@ -45,6 +47,7 @@ class Rectangle:
         self.tilt_angle = tilt_angle
         self.couleur = couleur
 
+    # méthode qui modifie la direction des rectangles par rapport aux contact avec les murs
     def update(self):
         self.x +=self.change_x
         self.y += self.change_y
@@ -58,9 +61,10 @@ class Rectangle:
         elif self.y > SCREEN_HEIGHT:
             self.change_y -= 2*self.change_y
 
-
+    # Méthode qui dessine les rectangles à l'écran
     def draw(self):
         arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.couleur, self.tilt_angle)
+
 
 class MyGame(arcade.Window):
     def __init__(self):
@@ -73,31 +77,33 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.BLACK)
         pass
 
+    # Méthode qui dessine les balles et rectangles contenues dans les listes d'objet
     def on_draw(self):
         arcade.start_render()
         for i in self.liste_de_balles:
             i.draw()
         for i in self.liste_de_rectangle:
             i.draw()
-
+    # Méthode qui fait l'update des balles et des rectangles
     def update(self, delta_time):
         for i in self.liste_de_balles:
             i.update()
 
         for i in self.liste_de_rectangle:
             i.update()
-
+    # Méthode capte l'action des touches de la souris et par rapport au input crée une balle ou un rectangle
     def on_mouse_press(self, x, y, button, modifiers):
 
         if button == arcade.MOUSE_BUTTON_LEFT:
             #for i in range(5):
-                ma_balle = Balle(x, y, 5, 10, 15, arcade.color.WHITE)
+                ma_balle = Balle(x, y, 5, 10, 15, arcade.color.DEEP_RUBY)
                 self.liste_de_balles.append(ma_balle)
 
         if button == arcade.MOUSE_BUTTON_RIGHT:
             #for i in range(5):
-                mon_rectangle = Rectangle(x, y, 25, 25, arcade.color.WHITE, 0)
+                mon_rectangle = Rectangle(x, y, 25, 25, arcade.color.FRENCH_BLUE, 0)
                 self.liste_de_rectangle.append(mon_rectangle)
+
 
 def main():
     my_game = MyGame()
